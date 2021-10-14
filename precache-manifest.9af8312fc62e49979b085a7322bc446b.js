578783fe6fcd4ca4a93fbb427120169dc2b77f15
unknown <you@example.com>
2021-10-14 14:39:12 +0400

    Updates

---
+++ precache-manifest.9af8312fc62e49979b085a7322bc446b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbcd8b66c3ba0a73e5b3011e91f2f5a2",
+    "url": "/crud-app/index.html"
+  },
+  {
+    "revision": "077ccd43d6aaffa00944",
+    "url": "/crud-app/static/css/2.eacbdd6c.chunk.css"
+  },
+  {
+    "revision": "d696ff45d8ee0ed31321",
+    "url": "/crud-app/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "077ccd43d6aaffa00944",
+    "url": "/crud-app/static/js/2.fa226f9e.chunk.js"
+  },
+  {
+    "revision": "d696ff45d8ee0ed31321",
+    "url": "/crud-app/static/js/main.b4fa0f4c.chunk.js"
+  },
+  {
+    "revision": "3b2bb94d963cf81ffa98",
+    "url": "/crud-app/static/js/runtime-main.7fd4aa3b.js"
+  }
+]);
